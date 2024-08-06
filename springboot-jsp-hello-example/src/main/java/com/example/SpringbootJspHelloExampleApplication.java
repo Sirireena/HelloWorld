7d161d10c4ba213e7491177f6f7eccb665877f10
Sirisiri <Sirisiri@DESKTOP-H18S30Q>
2024-08-06 12:45:44 +0530

    first commit

---
+++ springboot-jsp-hello-example/src/main/java/com/example/SpringbootJspHelloExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJspHelloExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspHelloExampleApplication.class, args);
+	}
+
+}
